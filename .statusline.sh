75000643db0c1abbce0252ca0806342dbb04e3b8
Henrik Hallberg <halhen@k2h.se>
2009-04-14 00:31:02 +0200

    .statusline.sh updated for statnot 0.0.2

--- .statusline.sh
+++ .statusline.sh
@@ -1,5 +1,10 @@
 loadavg="`cat /proc/loadavg | awk '{print $1, $2, $3}'`"
 mempercent="`free -b | awk 'NR == 2 { print int(0.5 + 1000 * ($3 - ($6 + $7)) / $2)/10}'`"
 batperstring="`acpi | awk '{print $4}' | sed s/,//g`"
+datestr="`date +'%R:%S'`"
 
-echo "[load ${loadavg}] [mem ${mempercent}%] [bat $batperstring]   `date +'%R'`"
+if [ $# -eq 0 ]; then
+    echo "[load ${loadavg}] [mem ${mempercent}%] [bat $batperstring]   $datestr"
+else
+    echo "[$1]   $datestr"
+fi
